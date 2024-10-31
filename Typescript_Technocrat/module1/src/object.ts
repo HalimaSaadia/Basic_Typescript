7b968138d2882c0413b0e21b97dc51e2b991fdfa
HalimaSaadia <halimasadia9666@gmail.com>
2024-10-31 21:16:41 +0600

    exploring reference type data type

---
+++ Typescript_Technocrat/module1/src/object.ts
@@ -0,0 +1,13 @@
+let user: {
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  company: "TTI", 
+  readonly course : "Next Level Web Development"
+} = {
+  firstName: "Halimea Tus",
+  lastName: "Sadia",
+  company: "TTI",
+  course: "Next Level Web Development"
+};
+console.log(user)
