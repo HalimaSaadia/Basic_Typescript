79b855e761dead28eaae1bf11778c83bfef96a36
HalimaSaadia <halimasadia9666@gmail.com>
2024-10-31 15:17:58 +0600

    explored data type

---
+++ Typescript_Technocrat/module1/src/1.4.ts
@@ -0,0 +1,8 @@
+let firstName : string = "Halima"
+let age : number = 24
+let isAdmin : boolean = false
+
+let friends : string[] = ["","" ]
+let elligibleRolls : number [] = [1, 2]
+
+let coordinates : ["", 1] = ["", 1]
