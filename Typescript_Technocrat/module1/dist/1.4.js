3f6a25348ad8d2f308df88c4312754fdbea0bf26
HalimaSaadia <halimasadia9666@gmail.com>
2024-10-31 21:21:10 +0600

    include js file for reference data type

---
+++ Typescript_Technocrat/module1/dist/1.4.js
@@ -0,0 +1,7 @@
+"use strict";
+let firstName = "Halima";
+let age = 24;
+let isAdmin = false;
+let friends = ["", ""];
+let elligibleRolls = [1, 2];
+let coordinates = ["", 1];
---
+++ Typescript_Technocrat/module1/dist/object.js
@@ -0,0 +1,7 @@
+"use strict";
+let user = {
+    firstName: "Halimea Tus",
+    lastName: "Sadia",
+    company: "TTI",
+    course: "Next Level Web Development"
+};
